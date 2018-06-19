f0a7b1726d25f3343ed14692da339f593b43e530
Cristian PS <cristian.pachacama01@gmail.com>
2018-06-19 12:48:49 -0500

    Grafico de Clima

---
+++ AppCluster/Code/DataClima.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(readr)
+library(dygraphs)
+
+dir = "Data/Clima Estaciones/Clima csv"
+estaciones = list.files(dir)
+
+BDD = read_delim(paste0(dir,"/",estaciones[10]), 
+                  ";", escape_double = FALSE, trim_ws = TRUE, 
+                  skip = 16)
+
+BDDts = ts(BDD[,-(1:3)],start = c(2000,1),frequency = 12)
+
+dygraphs::dygraph(BDDts[,1:3]) %>% 
+  dyRangeSelector(dateWindow = c("2000-01-01", "2015-12-01"))%>%
+  dyHighlight(highlightCircleSize = 5, 
+              highlightSeriesBackgroundAlpha = 0.6,
+              hideOnMouseOut = FALSE,
+              highlightSeriesOpts = list(strokeWidth = 1.5))
+
+
