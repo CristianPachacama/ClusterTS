ff72638fcaddad4300b9e4314eb2d7ec146bee12
Cristian PS <cristian.pachacama01@gmail.com>
2018-10-14 22:17:45 -0500

    Correccion Nombres y Cluster1

---
+++ Clusters Analizados/cluster0.R
@@ -0,0 +1,268 @@
+#------------------------     Cluster 1: Modelo Final     --------------------------
+library(dplyr)
+library(xts)
+library(TSA)
+library(forecast)
+library(ggplot2)
+library(dygraphs)
+library(TSstudio)
+
+library(lmtest)
+
+#Se elije Cluster y se compila codigo extra  ------------------------------
+nclu = 1
+source("Code/DistanciasAVazoes.R")
+source("Code/ACP Funcional.R")
+
+dir = "Data/Clima Estaciones/Clima csv2"
+estacionesExist = list.files(dir)
+
+# Juntamos Data de Vazoes, Clima mas cercano y Cluster
+VazClimClust = BDD_min %>% inner_join(Clusters) %>% arrange(Cluster)
+
+VariablesClima = VazClimClust %>% filter(Cluster==nclu)
+
+estacionesAux = unique(as.character(VariablesClima$Clima_min))
+
+BDDRegresion = MedVazPca
+
+for(k in  1:length(estacionesAux)){
+  
+  # k=1 #Estacion
+  indEst = estacionesExist == paste0(estacionesAux[k],".csv")
+  estacionK = estacionesExist[indEst]
+  print(paste("**************************",estacionK,"**************************"))
+  if(length(estacionK)>0){
+    
+    #Cargar Data de Clima asociadas al Cluster 
+    source("Code/DataClima.R")
+    #Limpiamos Data Clima
+    source("Code/LimpiezaSTL-Loess.R")
+    
+    #Juntamos en una sola Base (PCAVazoe, Clim1Esta1,Clima2Esta1,....Clima6Esta1)
+    LimBDDc = data.frame(LimBDDtsc)
+    estacion_k = substr(estacionK,1,nchar(estacionK)-4)
+    names(LimBDDc) = paste0(estacion_k,"_",names(LimBDDc))
+    LimBDDc$Fecha = as.Date(LimBDDtsc)
+    
+    BDDRegresion = BDDRegresion %>% inner_join(LimBDDc,by="Fecha")
+    
+  }
+  
+}
+
+#Vazoe obtenido del ACP Funcional 
+fecha0r = unlist(strsplit(x = as.character(BDDRegresion$Fecha[1]) , split = "-"))[-3]
+fecha0r = gsub("(?<![0-9])0+", "", fecha0r, perl = TRUE)
+fecha0r = as.numeric(fecha0r)
+
+
+VazoePCAts = ts(BDDRegresion$VazoePCA,start = fecha0r , frequency = 12)
+# library(TSstudio)
+# ts_plot(VazoePCAts)
+# ts_seasonal(VazoePCAts,type="all")
+# ts_lags(VazoePCAts, lags = c(12, 24, 36, 48))
+# ts_heatmap(VazoePCAts)
+
+#Removemos Variables ------------------------------------------------------
+remove(VazClimClust,VariablesClima,LimBDDc,LimBDDtsc,
+       estacion_k,estacionesAux,estacionK,indEst,k,BDDc)
+
+
+
+
+
+
+
+
+
+
+
+#Grafico de Series que componen la Base de Datos  -------------------------
+
+graf_series = function(BDDRegresion,ind = NULL,fecha = fecha0r){
+  
+  if(is.null(ind)){
+    aux = BDDRegresion[,-1]
+  }else{
+    aux = BDDRegresion[,ind]
+  }
+  
+  aux = ts(aux,start = fecha , frequency = 12)
+  
+  graf=dygraph(data = aux, main = "Datos Estacion Vazoes vs Clima")%>%
+    # dyAxis("x", label=names(metric)) %>%
+    # dyAxis('y',label='')%>%
+    dyHighlight(highlightSeriesBackgroundAlpha = 0.3)%>%
+    dyLegend(show= "follow",width = 400)
+  
+  return(graf)
+  
+}
+
+graf_series(BDDRegresion, fecha = fecha0r)
+# library(TSstudio)
+# ts_seasonal(BDDtsc[,3],type="all")
+# ts_lags(BDDtsc[,3], lags = c(12, 24, 36, 48))
+# ts_heatmap(BDDtsc[,3])
+
+
+
+
+
+# Indices Globales    -----------------------------------------------------------
+library(readxl)
+MACROclima <- read_excel("Data/MACRO Clima/MACROclima.xlsx",na="NA")
+
+
+MACROclima = as.data.frame(MACROclima)
+
+MACROclimaAux = MACROclima[(MACROclima$Año>=2000)&(MACROclima$Año<=2013),]
+MACROclimaAux = MACROclimaAux[-c(161:168),]
+dim(MACROclimaAux)
+dim(BDDRegresion)
+
+BDDRegMacro = cbind(BDDRegresion,MACROclimaAux[,-c(1,2)])
+
+#Grafico de Indices
+graf_series(BDDRegMacro, ind= c(37:40) ,fecha = fecha0r)
+
+
+#Grafico doble eje
+graf_series2 = function(BDDRegresion,ind = NULL,fecha = fecha0r){
+  
+  if(is.null(ind)){
+    aux = BDDRegresion[,-1]
+  }else{
+    aux = BDDRegresion[,c(2,ind)]
+  }
+  
+  aux = ts(aux,start = fecha , frequency = 12)
+  
+  graf=dygraph(data = aux, main = "Datos Estacion Vazoes vs Clima")%>%
+    dyHighlight(highlightSeriesBackgroundAlpha = 0.3) %>% 
+    dySeries("VazoePCA",axis = "y2")
+  
+  
+  return(graf)
+  
+}
+
+
+graf_series2(BDDRegMacro, ind= c(37:40) ,fecha = fecha0r)
+
+#SARIMAX con MACRO CLima --------------------------------------
+indTrain = -c((dim(BDDRegMacro)[1]-12):(dim(BDDRegMacro)[1]))
+BDDRegMacroTS = ts(BDDRegMacro,start=fecha0r,frequency = 12)
+
+BDDtrain = BDDRegMacroTS[indTrain ,]
+BDDtest = BDDRegMacroTS[-indTrain ,]
+
+VarXsMacro = names(BDDRegMacro)[c(4,13:21)]
+
+#Modelo usando funcion Arima
+ModLogArimaMacro = Arima(y= VazoePCAts[indTrain],
+                         order = c(p=4,d=0,q=1),
+                         seasonal = c(P=1,D=1,Q=0),
+                         xreg = BDDtrain[,c(VarXsMacro)]
+                         # include.constant = T,
+                         # include.mean = F
+)
+coeftest(ModLogArimaMacro)
+checkresiduals(ModLogArimaMacro)
+prediccion = forecast(ModLogArimaMacro,xreg = BDDtest[,c(VarXsMacro)])
+# accuracy(prediccion)
+autoplot(prediccion)
+
+
+#Variante del modelo (arima) Admite Estacionalidad (No Vale AUTOPLOT ni FORECAST)
+# ModLogArimaMacro = TSA::arima(x= log(VazoePCAts[indTrain]),
+#                               order = c(p=4,d=0,q=1),
+#                               seasonal = list(order= c(P=1,D=1,Q=0),period=12),
+#                               xreg = BDDtrain[,c(VarXsMacro)],
+#                               include.mean = T
+# )
+# coeftest(ModLogArimaMacro)
+# checkresiduals(ModLogArimaMacro)
+# prediccion = forecast(ModLogArimaMacro,xreg = BDDtest[,c(VarXsMacro)])
+# # accuracy(prediccion)
+# autoplot(prediccion)
+
+#Grafico de Predicciones
+test_forecast(actual = VazoePCAts, forecast.obj = prediccion, test = BDDtest[,c("VazoePCA")])
+
+
+# ts_seasonal(BDDtsc[,3],type="all")
+# ts_lags(BDDtsc[,3], lags = c(12, 24, 36, 48))
+# ts_heatmap(BDDtsc[,3])
+
+
+# #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# # Modelo Primero (Sin considerar Data Particionada) --------------------------
+# #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# VarXsMacro = names(BDDRegMacro)[c(4,5,6,8,12,13,14)]
+# 
+# #Modelo usando funcion Arima ------------------------------------
+# BDDRegMacroTS = ts(BDDRegMacro,start = fecha0r , frequency = 12)
+# modelo0 = Arima(y= log(VazoePCAts),
+#                 order = c(p=6,d=0,q=4),
+#                 seasonal = c(P=1,D=1,Q=0),
+#                 xreg = log(BDDRegMacroTS[,c(VarXsMacro)])
+#                 # include.constant = T,
+#                 # include.mean = F
+# )
+# 
+# summary(modelo0)
+# coeftest(modelo0)
+# checkresiduals(modelo0)
+# prediccion = forecast(modelo0,xreg = log(BDDRegMacroTS[150:160,c(VarXsMacro)]))
+# autoplot(prediccion)
+# # accuracy(prediccion)
+# 
+# 
+# 
+# 
+# #Modelado con funcion "arima" ---------------------------------
+# BDDRegMacroTS = ts(BDDRegMacro,start = fecha0r , frequency = 12)
+# modelo1 = arima(x= log(VazoePCAts),
+#                 order = c(p=6,d=0,q=4),
+#                 seasonal = c(P=1,D=1,Q=0),
+#                 xreg = log(BDDRegMacroTS[,c(VarXsMacro)])
+# )
+# 
+# summary(modelo1)
+# coeftest(modelo1)
+# checkresiduals(modelo1)
+# prediccion = forecast(modelo1,xreg = log(BDDRegMacroTS[,c(VarXsMacro)]))
+# prediccion = predict(modelo1,newxreg = log(BDDRegMacroTS[,c(VarXsMacro)]))
+# 
+# autoplot(prediccion)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Clusters Analizados/cluster1.R
+++ Clusters Analizados/cluster1.R
@@ -1,251 +1,89 @@
-#------------------------     Cluster 1: Modelo Final     --------------------------
+#------------------------     Cluster 2: Modelo Final     --------------------------
 library(dplyr)
+
 library(xts)
 library(TSA)
 library(forecast)
+
 library(ggplot2)
 library(dygraphs)
 library(TSstudio)
 
+library(lubridate)
 library(lmtest)
 
 #Se elije Cluster y se compila codigo extra  ------------------------------
 nclu = 1
 source("Code/DistanciasAVazoes.R")
 source("Code/ACP Funcional.R")
-
-dir = "Data/Clima Estaciones/Clima csv2"
-estacionesExist = list.files(dir)
-
-# Juntamos Data de Vazoes, Clima mas cercano y Cluster
-VazClimClust = BDD_min %>% inner_join(Clusters) %>% arrange(Cluster)
-
-VariablesClima = VazClimClust %>% filter(Cluster==nclu)
-
-estacionesAux = unique(as.character(VariablesClima$Clima_min))
-
-BDDRegresion = MedVazPca
-
-for(k in  1:length(estacionesAux)){
-  
-  # k=1 #Estacion
-  indEst = estacionesExist == paste0(estacionesAux[k],".csv")
-  estacionK = estacionesExist[indEst]
-  print(paste("**************************",estacionK,"**************************"))
-  if(length(estacionK)>0){
-    
-    #Cargar Data de Clima asociadas al Cluster 
-    source("Code/DataClima.R")
-    #Limpiamos Data Clima
-    source("Code/LimpiezaSTL-Loess.R")
-    
-    #Juntamos en una sola Base (PCAVazoe, Clim1Esta1,Clima2Esta1,....Clima6Esta1)
-    LimBDDc = data.frame(LimBDDtsc)
-    estacion_k = substr(estacionK,1,nchar(estacionK)-4)
-    names(LimBDDc) = paste0(estacion_k,"_",names(LimBDDc))
-    LimBDDc$Fecha = as.Date(LimBDDtsc)
-    
-    BDDRegresion = BDDRegresion %>% inner_join(LimBDDc,by="Fecha")
-    
-  }
-  
-}
-
-#Vazoe obtenido del ACP Funcional 
-fecha0r = unlist(strsplit(x = as.character(BDDRegresion$Fecha[1]) , split = "-"))[-3]
-fecha0r = gsub("(?<![0-9])0+", "", fecha0r, perl = TRUE)
-fecha0r = as.numeric(fecha0r)
-
-
-VazoePCAts = ts(BDDRegresion$VazoePCA,start = fecha0r , frequency = 12)
-# library(TSstudio)
-# ts_plot(VazoePCAts)
-# ts_seasonal(VazoePCAts,type="all")
-# ts_lags(VazoePCAts, lags = c(12, 24, 36, 48))
-# ts_heatmap(VazoePCAts)
-
-#Removemos Variables ------------------------------------------------------
-remove(VazClimClust,VariablesClima,LimBDDc,LimBDDtsc,
-       estacion_k,estacionesAux,estacionK,indEst,k,BDDc)
-
-
-
-
-
-
-
-
-
-
-
-#Grafico de Series que componen la Base de Datos  -------------------------
-
-graf_series = function(BDDRegresion,ind = NULL,fecha = fecha0r){
-  
-  if(is.null(ind)){
-    aux = BDDRegresion[,-1]
-  }else{
-    aux = BDDRegresion[,ind]
-  }
-  
-  aux = ts(aux,start = fecha , frequency = 12)
-  
-  graf=dygraph(data = aux, main = "Datos Estacion Vazoes vs Clima")%>%
-    # dyAxis("x", label=names(metric)) %>%
-    # dyAxis('y',label='')%>%
-    dyHighlight(highlightSeriesBackgroundAlpha = 0.3)%>%
-    dyLegend(show= "follow",width = 400)
-  
-  return(graf)
-  
-}
-
-graf_series(BDDRegresion, fecha = fecha0r)
-# library(TSstudio)
-# ts_seasonal(BDDtsc[,3],type="all")
-# ts_lags(BDDtsc[,3], lags = c(12, 24, 36, 48))
-# ts_heatmap(BDDtsc[,3])
-
-
-
-
-
-# Indices Globales    -----------------------------------------------------------
-library(readxl)
-MACROclima <- read_excel("Data/MACRO Clima/MACROclima.xlsx",na="NA")
-
-
-MACROclima = as.data.frame(MACROclima)
-
-MACROclimaAux = MACROclima[(MACROclima$Año>=2000)&(MACROclima$Año<=2013),]
-MACROclimaAux = MACROclimaAux[-c(161:168),]
-dim(MACROclimaAux)
-dim(BDDRegresion)
-
-BDDRegMacro = cbind(BDDRegresion,MACROclimaAux[,-c(1,2)])
-
-#Grafico de Indices
-graf_series(BDDRegMacro, ind= c(37:40) ,fecha = fecha0r)
-
-
-#Grafico doble eje
-graf_series2 = function(BDDRegresion,ind = NULL,fecha = fecha0r){
-  
-  if(is.null(ind)){
-    aux = BDDRegresion[,-1]
-  }else{
-    aux = BDDRegresion[,c(2,ind)]
-  }
-  
-  aux = ts(aux,start = fecha , frequency = 12)
-  
-  graf=dygraph(data = aux, main = "Datos Estacion Vazoes vs Clima")%>%
-    dyHighlight(highlightSeriesBackgroundAlpha = 0.3) %>% 
-    dySeries("VazoePCA",axis = "y2")
-  
-  
-  return(graf)
-  
-}
-
-
-graf_series2(BDDRegMacro, ind= c(37:40) ,fecha = fecha0r)
+source("Code/BasesVazClimMin.R")
+source("Code/BddMacro.R")
 
 #SARIMAX con MACRO CLima --------------------------------------
-indTrain = -c((dim(BDDRegMacro)[1]-12):(dim(BDDRegMacro)[1]))
-BDDRegMacroTS = ts(BDDRegMacro,start=fecha0r,frequency = 12)
+particion = 0.20 #Particion 20% - 80%
 
-BDDtrain = BDDRegMacroTS[indTrain ,]
+ntr = round(particion*dim(BDDRegMacro)[1])
+indTrain = -c((dim(BDDRegMacro)[1]-ntr):(dim(BDDRegMacro)[1]))
+BDDRegMacroTS = ts(BDDRegMacro,start=fecha0r,frequency = 12)
+# BDDRegMacroTS =BDDRegMacro
+
+BDDtrain = ts(BDDRegMacroTS[indTrain ,],start=fecha0r,frequency = 12)
 BDDtest = BDDRegMacroTS[-indTrain ,]
 
-VarXsMacro = names(BDDRegMacro)[c(4,13:21)]
+#SELECCION DE VARIABLES  -------------------------------------
+(VarXsMacro = names(BDDRegMacro)[c(4,5,6,8,16,17,18,36)])
+
+(VarXsMacro = names(BDDRegMacro)[c(6)])
+
+(VarXsMacro = names(BDDRegMacro)[c(37,38)])
+
+X  = BDDtrain[,c(VarXsMacro)]
+VazTrain = ts(VazoePCAts[indTrain],start=fecha0r,frequency = 12)
+# VazTrain = ts(BDDtsv[(12*60):(12*60+126),1],start=fecha0r,frequency = 12)
+# VazActual = ts(BDDtsv[(12*60):(12*60+159),1],start=fecha0r,frequency = 12)
 
 #Modelo usando funcion Arima
-ModLogArimaMacro = Arima(y= VazoePCAts[indTrain],
-                         order = c(p=4,d=0,q=1),
-                         seasonal = c(P=1,D=1,Q=0),
-                         xreg = BDDtrain[,c(VarXsMacro)]
-                         # include.constant = T,
-                         # include.mean = F
+# ArimaF = Arima
+# arimaf = arimax
+modeloClust = Arima(y= log(VazTrain),
+                    order = c(p=6,d=0,q=2),
+                    seasonal = list(order = c(1,1,0), period = 12),
+                    xreg = X
 )
-coeftest(ModLogArimaMacro)
-checkresiduals(ModLogArimaMacro)
-prediccion = forecast(ModLogArimaMacro,xreg = BDDtest[,c(VarXsMacro)])
+
+summary(modeloClust)
+coeftest(modeloClust)
+checkresiduals(modeloClust)
+prediccion = forecast(modeloClust,xreg = BDDtest[,c(VarXsMacro)])
 # accuracy(prediccion)
 autoplot(prediccion)
+test_forecast(actual = log(VazoePCAts), forecast.obj = prediccion, test = BDDtest[,"VazoePCA"])
+
+# test_forecast(actual = log(VazActual), forecast.obj = prediccion, test = BDDtest[,c(VarXsMacro)])
+
+
+
+
+
+
 
 
 #Variante del modelo (arima) Admite Estacionalidad (No Vale AUTOPLOT ni FORECAST)
-# ModLogArimaMacro = TSA::arima(x= log(VazoePCAts[indTrain]),
+# (VarXsMacro = names(BDDRegMacro)[c(6,7,11)])
+# X  = BDDtrain[,c(VarXsMacro)]
+# modeloClust = TSA::arima(x= VazoePCAts[indTrain],
 #                               order = c(p=4,d=0,q=1),
 #                               seasonal = list(order= c(P=1,D=1,Q=0),period=12),
-#                               xreg = BDDtrain[,c(VarXsMacro)],
+#                               xreg = X ,
 #                               include.mean = T
 # )
-# coeftest(ModLogArimaMacro)
-# checkresiduals(ModLogArimaMacro)
-# prediccion = forecast(ModLogArimaMacro,xreg = BDDtest[,c(VarXsMacro)])
-# # accuracy(prediccion)
+# summary(modeloClust)
+# coeftest(modeloClust, xreg = )
+# checkresiduals(modeloClust)
+# prediccion = forecast(modeloClust,xreg = BDDtest[,c(VarXsMacro)])
 # autoplot(prediccion)
 
 #Grafico de Predicciones
-test_forecast(actual = VazoePCAts, forecast.obj = prediccion, test = BDDtest[,c("VazoePCA")])
-
-
-# ts_seasonal(BDDtsc[,3],type="all")
-# ts_lags(BDDtsc[,3], lags = c(12, 24, 36, 48))
-# ts_heatmap(BDDtsc[,3])
-
-
-# #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# # Modelo Primero (Sin considerar Data Particionada) --------------------------
-# #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# VarXsMacro = names(BDDRegMacro)[c(4,5,6,8,12,13,14)]
-# 
-# #Modelo usando funcion Arima ------------------------------------
-# BDDRegMacroTS = ts(BDDRegMacro,start = fecha0r , frequency = 12)
-# modelo0 = Arima(y= log(VazoePCAts),
-#                 order = c(p=6,d=0,q=4),
-#                 seasonal = c(P=1,D=1,Q=0),
-#                 xreg = log(BDDRegMacroTS[,c(VarXsMacro)])
-#                 # include.constant = T,
-#                 # include.mean = F
-# )
-# 
-# summary(modelo0)
-# coeftest(modelo0)
-# checkresiduals(modelo0)
-# prediccion = forecast(modelo0,xreg = log(BDDRegMacroTS[150:160,c(VarXsMacro)]))
-# autoplot(prediccion)
-# # accuracy(prediccion)
-# 
-# 
-# 
-# 
-# #Modelado con funcion "arima" ---------------------------------
-# BDDRegMacroTS = ts(BDDRegMacro,start = fecha0r , frequency = 12)
-# modelo1 = arima(x= log(VazoePCAts),
-#                 order = c(p=6,d=0,q=4),
-#                 seasonal = c(P=1,D=1,Q=0),
-#                 xreg = log(BDDRegMacroTS[,c(VarXsMacro)])
-# )
-# 
-# summary(modelo1)
-# coeftest(modelo1)
-# checkresiduals(modelo1)
-# prediccion = forecast(modelo1,xreg = log(BDDRegMacroTS[,c(VarXsMacro)]))
-# prediccion = predict(modelo1,newxreg = log(BDDRegMacroTS[,c(VarXsMacro)]))
-# 
-# autoplot(prediccion)
-
-
-
-
-
-
-
-
-
 
 
 
